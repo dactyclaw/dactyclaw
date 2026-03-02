fe6b61cde89ce142fbdff4ba53d9cf98d00f6787
DECTYCLAW <263858794+dectyclaw@users.noreply.github.com>
2026-03-03 04:51:48 +0700

    fix: remove BOM from bin/dectyclaw.js - fixes shebang on Linux

--- bin/dectyclaw.js
+++ bin/dectyclaw.js
@@ -1,4 +1,4 @@
-﻿#!/usr/bin/env node
+#!/usr/bin/env node
 const { program } = require('commander');
 const { ethers } = require('ethers');
 const { createPublicClient, createWalletClient, http, formatEther } = require('viem');
